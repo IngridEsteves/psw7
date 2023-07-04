4918e94779288f6dcf7257f9be0a0289f469780f
Ingrid Esteves <ingridestevespereiraiep@gmail.com>
2023-07-04 20:29:21 -0300

    aula1-part2

--- perfil/urls.py
+++ perfil/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('gerenciar/', views.gerenciar, name="gerenciar"),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def gerenciar(request):
+    return render(request, 'gerenciar.html')
