17b56df59ea5c45301f251249bde7c4647dc8e6d
Ingrid Esteves <ingridestevespereiraiep@gmail.com>
2023-07-08 12:25:10 -0300

    aula2-part4

--- core/urls.py
+++ core/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('perfil/', include('perfil.urls')),
     path('extrato/', include('extrato.urls')),
+    path('planejamento/', include('planejamento.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
